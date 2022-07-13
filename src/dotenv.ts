7fb432fc9ab199f31a3818b790f246eab1492cbd
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2022-07-13 22:27:47 +0800

    Initial commit

---
+++ src/dotenv.ts
@@ -0,0 +1,79 @@
+export interface ParseResult {
+  document?: string[];
+  inline?: string;
+  key: string;
+  value?: string;
+  type: string;
+}
+
+/**
+ * Regex to capture key value pair with inline comment
+ */
+const lineWithInlineComment = () =>
+  /(?:^|^)\s*(?:export\s+)?([\w.-]+)(?:\s*=\s*?|:\s+?)(\s*'(?:\\'|[^'])*'|\s*"(?:\\"|[^"])*"|\s*`(?:\\`|[^`])*`|[^#\r\n]+)?\s*((?:#.*)?(?:$|$))/gim;
+
+const booleanLike = new Set([
+  "yes",
+  "y",
+  "1",
+  "true",
+  "on",
+  "no",
+  "n",
+  "0",
+  "false",
+  "off",
+]);
+
+/**
+ * parses env content
+ *
+ * inspired from: {@link https://github.com/motdotla/dotenv/blob/228c7b449dd9adfa93447ba74c8bf894db3068e5/lib/main.js#L8 source}
+ * @param src env content
+ *
+ */
+export function parseEnv(src: string | Buffer) {
+  const result: Record<string, ParseResult> = {};
+
+  // Convert buffer to string
+  const linesString = src.toString().replace(/\r\n?/gm, "\n");
+
+  const lines = linesString.split(/\n/gm);
+
+  let reuseableDocument: string[] = [];
+
+  for (const line of lines) {
+    const trimmed = line.trimEnd();
+    if (!trimmed.length) continue;
+    if (trimmed.match(/^\s*#/g)) {
+      reuseableDocument.push(trimmed.replace(/\s*#/g, "*"));
+    } else {
+      const regex = lineWithInlineComment();
+      let m: RegExpExecArray | null;
+      if ((m = regex.exec(trimmed)) !== null) {
+        const [, key, value, inline] = m;
+
+        const trimmedValue = value.trim();
+
+        let type = "string";
+        if (!isNaN(+trimmedValue)) {
+          type = trimmedValue.includes(".") ? "double" : "integer";
+        } else if (booleanLike.has(trimmedValue.toLocaleLowerCase("en-gb"))) {
+          type = "boolean";
+        }
+
+        result[key] = {
+          document: reuseableDocument.length ? [...reuseableDocument] : void 0,
+          inline: inline.replace(/#/g, "//") || void 0,
+          key,
+          value,
+          type,
+        };
+
+        reuseableDocument.length = 0;
+      }
+    }
+  }
+
+  return Object.values(result);
+}
---
+++ src/index.test.ts
@@ -0,0 +1,29 @@
+import { parseEnv } from "./dotenv";
+
+// fixtures
+const plainEnv = `
+KEY=value
+KEY2=value
+`;
+
+const documentedEnv = `
+# this is a string
+STR_KEY=str
+
+# this is a number
+NUM_KEY=2
+
+# this is a boolean
+
+BOOL_KEY=off
+
+#this has no space after #
+HELLO=world
+`;
+
+const documentedWithInline = `
+# this has document
+KEY=value #inline
+`;
+
+console.log({ documentedWithInline, r: parseEnv(documentedWithInline) });
